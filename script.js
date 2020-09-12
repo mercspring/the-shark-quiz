f5399a3f41b79499d1cefa7f911c1e7f798b9c71
mercspring <68706508+mercspring@users.noreply.github.com>
2020-09-12 13:13:44 -0700

    created functions to generate and remove my quizz items

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
-var questions = [{ questionText: "What color is my hair?", answer1: "brown", answer2: "red", answer3: "blonde", answer4: "grey", correctAnswer: "brown" },
-{ questionText: "What is my dogs name?", answer1: "Hen", answer2: "hex", answer3: "hover", answer4: "dodo", correctAnswer: this.answer1 },
+var questions = [{ questionText: "What color is my hair?", answers: ["Brown", "Red", "Blonde", "Gray"], correctAnswer: "Brown" },
+{ questionText: "What is my dogs name?", answers: ["Hex", "Hen", "Dodo", "Almond"], correctAnswer: "Hex" },
 { questionText: "What state do I live in?", answers: ["WA", "OR", "UT", "ID"], correctAnswer: "WA" }]
 
 
-var k = 0;
-// var answerBtn0 = document.querySelector("#answer-0");
-// var answerBtn0 = document.querySelector("#answer-1");
-// var answerBtn0 = document.querySelector("#answer-2");
-// var answerBtn0 = document.querySelector("#answer-3");
 
-for (i = 0; i < questions[2].answers.length; i++) {
-    var button = document.createElement("button");
-    button.setAttribute("class", "btn quiz-answer btn-color");
-    button.setAttribute("id", `answer-${i}`);
-    button.innerText = questions[2].answers[i];
-    if (k === 0) {
-        document.querySelector(".column-1").appendChild(button)
-        k = 1;
-    } else {
-        document.querySelector(".column-2").appendChild(button)
-        k = 0;
+function cleanUpQuiz(questionNumber){
+        document.querySelector(".question").remove();
+    for (i = 0; i < questions[questionNumber].answers.length; i++) {
+        document.querySelector(`#answer-${i}`).remove();
     }
+}
+function populateQuiz(questionNumber) {
+    var question = document.createElement("p");
+    question.innerText = questions[questionNumber].questionText;
+    question.setAttribute("class", "question");
+    document.querySelector(`main`).appendChild(question);
+    for (i = 0; i < questions[questionNumber].answers.length; i++) {
+        var button = document.createElement("button");
+        button.setAttribute("class", "quiz-answer btn-color");
+        button.setAttribute("id", `answer-${i}`);
+        button.innerText = questions[questionNumber].answers[i];
+
+        document.querySelector(`main`).appendChild(button);
+
+    }
+}
+
+function generateQuestions(quizLength) {
+    var quizItems = [];
+    var i = quizLength;
+    var j;
+    while (i > 0) {
+        j = randomWhole(quizLength)
+        if (quizItems.indexOf(j) === -1) {
+            quizItems.push(j);
+            i--;
+        }
+    }
+
+    return quizItems
 
 }
 
+
+function randomWhole(max) {
+    return Math.floor(Math.random() * max);
+}
 
 
